d776c5727799137f0f9dd12a99f3efa7d4d728ea
Matthew Strenk <strenkml@users.noreply.github.com>
2018-04-10 13:24:54 -0400

    Add print for type of benchmark

--- fullSim.py
+++ fullSim.py
@@ -5,12 +5,16 @@ goodInput = True
 
 simType = input("Welcome to the multicore scratchpad and cache simulator!\nPlease choose an option for memory simulation by entering the corresponding number \n1.Hash\n2.Heap\n3.Stride\n4.Trace\n")
 if int(simType) == 1:
+    print("Now running a hash benchmark")
     os.system("fullSim.bat hash")
 elif int(simType) == 2:
+    print("Now running a heap benchmark")
     os.system("fullSim.bat heap")
 elif int(simType) == 3:
+    print("Now running a stride benchmark")
     os.system("fullSim.bat stride")
 elif int(simType) == 4:
+    print("Now running a trace benchmark")
     os.system("fullSim.bat 4")
 else:
     print("You did not chose an appropriate number. Please try 1, 2, 3, or 4\n")
